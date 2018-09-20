302b75708dbaf9409feb4b8c8cadebf1afac7b5f
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-20 10:20:13 -0400

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ pkg_name='ml_spikeforest'
 
 setuptools.setup(
     name=pkg_name,
-    version="0.1.1",
+    version="0.1.2",
     author="Jeremy Magland",
     author_email="",
     description="MountainLab package with spike sorting utilities",
